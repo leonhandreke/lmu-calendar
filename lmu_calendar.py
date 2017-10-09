c0b4f63791334dfd5362bbaefa8b5cefe5b28bf2
Leon Handreke <leonh@ndreke.de>
2017-10-09 08:51:30 +0200

    Add LMU Calendar proxy

---
+++ lmu_calendar.py
@@ -0,0 +1,31 @@
+import requests
+import flask
+import lxml.html
+
+
+app = flask.Flask(__name__)
+
+@app.route("/stundenplan/<semester_id>")
+def get_stundenplan(semester_id):
+    username = flask.request.args.get("username")
+    password = flask.request.args.get("password")
+
+    if not (username and password):
+        flask.abort(403)
+
+    session = requests.Session()
+    session.post("https://lsf.verwaltung.uni-muenchen.de/qisserver/rds?state=user&type=1&category=auth.login&startpage=portal.vm&breadCrumbSource=portal",
+                 data={"asdf": username, "fdsa": password})
+
+    # Register requested semester in session
+    session.get("https://lsf.verwaltung.uni-muenchen.de/qisserver/rds?state=user&type=0&k_semester.semid={semester_id}&idcol=k_semester.semid&idval={semester_id}&purge=n&getglobal=semester".format(semester_id=semester_id))
+
+    r = session.get("https://lsf.verwaltung.uni-muenchen.de/qisserver/rds?state=wplan&act=show&show=plan&P.subc=plan&navigationPosition=functions%2CscheduleLoggedin&breadcrumb=schedule&topitem=functions&subitem=scheduleLoggedin")
+    page = lxml.html.fromstring(r.content)
+
+    ical_download_buttons = page.xpath('.//a[contains(text(), "Stundenplan als iCal-Datei herunterladen")]')
+    if not ical_download_buttons:
+        return '', 204  # No Content
+
+    ical_response = session.get(ical_download_buttons[0].get("href"))
+    return ical_response.content
